9a7bf30eaa42fe0d17fd453a9c56c8b3eb395123
Ben Hanna <benpaulhanna@gmail.com>
2018-06-27 17:57:49 +0000

    Added security to document functions.

--- api/routes/users.py
+++ api/routes/users.py
@@ -1,14 +1,15 @@
 from bottle import get, HTTPError, request, response
 
+from api.helpers import get_user_id
 from api.middleware import authenticated
 import lib.users as users
 
 @get('/users/me')
 @authenticated
 def me():
-    session = request.enivron.get('beaker.session')
+    user_id = get_user_id()
     
     try:
-        return users.get(session['user_id'])
+        return users.get(user_id)
     except Exception:
         raise HTTPError(status=404)
--- lib/documents.py
+++ lib/documents.py
@@ -1,11 +1,17 @@
 from lib.models import Document, Element
 
+def create(document):
+    return Document.create(**document)
+
+def create_element(user_id, element):
+    document = get(user_id, element.document_id)
+    return Element.create(**element)
+
 def find(user_id):
     return (
         Document
         .select()
         .where(Document.owner == user_id)
-        .dicts()
     )
     
 def get(user_id, document_id):
@@ -22,4 +28,20 @@ def get_elements(user_id, document_id):
         .select()
         .join(Document)
         .where(Document.id == document_id & Document.owner == user_id)
+    )
+    
+def update_document(user_id, document_id, document):
+    return (
+        Document
+        .update(**document)
+        .where(Document.id == document_id & Document.owner == user_id)
+    )
+    
+def update_element(user_id, document_id, element):
+    return (
+        Element
+        .update(**element)
+        .join(Document)
+        .where(Document.id == document_id & Document.owner == user_id)
+        .execute()
     )
