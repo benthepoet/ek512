615d5e298d2e94ad3638ada331ee0f812bf35846
Ben Hanna <benpaulhanna@gmail.com>
2018-06-28 19:27:59 +0000

    Moved routes.

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ import bottle
 import uuid
 from beaker.middleware import SessionMiddleware
 
-from api.routes import auth, documents, users
+from routes import auth, documents, users
 
 session_opts = {
     'session.type': 'cookie',
