936f9d95b267e5e8f7a5e5122062ed8f5d1c6684
Ben Hanna <benpaulhanna@gmail.com>
2018-06-29 15:43:15 +0000

    Fixed seed.

--- data/seed.py
+++ data/seed.py
@@ -1,27 +1,19 @@
 from lib.models import *
-import lib.auth as auth
+import lib.security as security
 
 def run():
     editor_role = Role.create(name='Editor')
     
-    permissions = [
-        { 'name': 'document.read' },
-        { 'name': 'document.write' },
-        { 'name': 'document.element.read' },
-        { 'name': 'document.element.write' }
-    ]
-    
-    for data in permissions:
-        permission = Permission.create(**data)
-        RolePermission.create(role=editor_role.id, permission=permission.id)
-    
-    editor_id = auth.register(email='editor@home.com', password='abc123')
+    editor_id = security.create_user(email='editor@home.com', password='abc123')
     editor = User[editor_id]
     editor.save()
     
     UserRole.create(user=editor, role=editor_role)
     
-    superuser_id = auth.register(email='superuser@home.com', password='def456')
+    superuser_role = Role.create(name='Superuser')
+    
+    superuser_id = security.create_user(email='superuser@home.com', password='def456')
     superuser = User[superuser_id]
-    superuser.is_superuser = True
     superuser.save()
+    
+    UserRole.create(user=superuser, role=superuser_role)
