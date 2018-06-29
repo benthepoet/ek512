8357455dca281767cabeb10172e8ef32b1c59e95
Ben Hanna <benpaulhanna@gmail.com>
2018-06-29 21:00:35 +0000

    Fixed broken tests.

--- tests/functional.py
+++ tests/functional.py
@@ -3,20 +3,24 @@ from webtest import AppError, TestApp
 
 import app as main
 
+app = TestApp(main.app)
+
 class TestResources(unittest.TestCase):
 
     def test_login(self):
-        app = TestApp(main.app)
         params = dict(email='editor@home.com', password='abc123')
         response = app.post_json('/auth/login', params)
         self.assertEqual(response.status_code, 200)
         
     def test_signup(self):
-        app = TestApp(main.app)
-        
         params = dict(email='user@home.com', password='ghi789')
-        good_response = app.post_json('/auth/signup', params)
-        self.assertEqual(good_response.status_code, 204)
+        response = app.post_json('/auth/signup', params)
+        self.assertEqual(response.status_code, 204)
+        
+    def test_signup_existing(self):
+        params = dict(email='editor@home.com', password='ghi789')
+        response = app.post_json('/auth/signup', params, status=409)
+        self.assertEqual(response.status_code, 409)
 
 if __name__ == '__main__':
     unittest.main()
