057dce8ff53c193746300ba5e4408094ae0091d5
Ben Hanna <benpaulhanna@gmail.com>
2018-07-05 14:35:53 +0000

    Removed redundant error handling.

--- resources/users.py
+++ resources/users.py
@@ -6,7 +6,4 @@ import lib.security as security
 class Me(object):
     @helpers.authorize
     def on_get(self, req, resp, user_id):
-        try:
-            resp.body = helpers.to_json(security.get_user(user_id))
-        except Exception:
-            raise falcon.HTTPError(falcon.HTTP_404)
+        resp.body = helpers.to_json(security.get_user(user_id))
