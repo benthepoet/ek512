b70e9824f5f2c87c829d251cb941430b7c09fb69
Ben Hanna <benpaulhanna@gmail.com>
2018-07-05 12:29:29 +0000

    Cleaned up documents module.

--- lib/documents.py
+++ lib/documents.py
@@ -1,20 +1,8 @@
+import falcon
+
 from playhouse.shortcuts import model_to_dict, update_model_from_dict
 
 from lib.models import Document, Element
-
-def select_all(user_id):
-    return (
-        Document
-        .select()
-        .where(Document.owner == user_id)
-    )
-    
-def select_one(user_id, document_id):
-    return (
-        select_all(user_id)
-        .where(Document.id == document_id)
-        .get()
-    )
 
 def create(**data):
     document = Document.create(**data)
@@ -25,31 +13,61 @@ def create_element(data):
     return model_to_dict(element, recurse=False)
 
 def find(user_id):
-    return list(select_all(user_id).dicts())
+    return list(select_documents(user_id).dicts())
     
 def find_elements(user_id, document_id):
     return list(
-        select_one(user_id, document_id)
+        select_document(user_id, document_id)
         .elements
         .dicts()
     )
     
 def get(user_id, document_id):
-    return model_to_dict(select_one(user_id, document_id), recurse=False)
+    document = select_document(user_id, document_id)
+    return model_to_dict(document, recurse=False)
+
+def select_documents(user_id):
+    return (
+        Document
+        .select()
+        .where(Document.owner == user_id)
+    )
+    
+def select_document(user_id, document_id):
+    try:
+        document = (
+            select_documents(user_id)
+            .where(Document.id == document_id)
+            .get()
+        )
+    except Document.DoesNotExist:
+        raise falcon.HTTPNotFound(description='The document does not exist.')
+        
+    return document
+    
+def select_element(user_id, document_id, element_id):
+    document = select_document(user_id, document_id)
+    
+    try:
+        element = (
+            document
+            .elements
+            .where(Element.id == element_id)
+            .get()
+        )
+    except Element.DoesNotExist:
+        raise falcon.HTTPNotFound(description='The element does not exist.')
+        
+    return element
 
 def update(user_id, document_id, data):
-    document = select_one(user_id, document_id)
+    document = select_document(user_id, document_id)
     update_model_from_dict(document, data)
     document.save()
     return model_to_dict(document, recurse=False)
 
 def update_element(user_id, document_id, element_id, data):
-    element = (
-        select_one(user_id, document_id)
-        .elements
-        .where(Element.id == element_id)
-        .get()
-    )
+    element = select_element(user_id, document_id, element_id)
     update_model_from_dict(element, data)
     element.save()
     return model_to_dict(element, recurse=False)
